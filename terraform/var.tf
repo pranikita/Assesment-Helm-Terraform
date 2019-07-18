5bb014ff03182530e16a2bd48612381da9ed9774
root <root@ip-172-31-41-73.ec2.internal>
2019-07-18 06:49:33 +0000

    Assesment for terraform

---
+++ terraform/var.tf
@@ -0,0 +1,15 @@
+variable "aws_vpc" {
+    description = "VPC"
+    default = "vpc-5efb6324"
+}
+
+variable "aws_subnet" {
+    description = "aws subnet"
+    default = "subnet-959ed7c9"
+}
+
+variable "aws_key" {
+    description = "aws key"
+    default = "aws_lab_key"
+}
+
